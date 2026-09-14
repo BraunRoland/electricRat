ced5d8358969bca3503c7d5a312d386c83c5e25b
BraunRoland <braun.roland@petrik.hu>
2026-09-14 09:01:51 +0200

    form

--- test/rat.test.ts
+++ test/rat.test.ts
@@ -8,13 +8,13 @@ describe("Rat osztály", () => {
     test("Üres név", () => {
         expect(() => {new Rat("",11,70)}).to.throw();
     });
-    test("kevéss atk", () => {
+    test("kevés atk", () => {
         expect(() => {new Rat("Atis",9,70)}).to.throw();
     });
     test("sok atk", () => {
         expect(() => {new Rat("Rózsi",50,61)}).to.throw();
     })
-    test("kevéss hp", () => {
+    test("kevés hp", () => {
         expect(() => {new Rat("Atis",15,49)}).to.throw();
     });
     test("sok hp", () => {
