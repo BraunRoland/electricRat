7bd619e16f35fce1f6235e4b705b870b02a97b56
BraunRoland <braun.roland@petrik.hu>
2026-09-14 08:48:02 +0200

    rat.test

---
+++ src/rat.ts
@@ -0,0 +1,20 @@
+export class Rat {
+    name: string;
+    atk: number;
+    hp: number;
+    constructor(name: string, atk: number, hp: number) {
+        if (name == undefined || name == null || name == "") {
+            throw new Error("Nem lehet üres a név!");
+        }
+        else if (atk < 10 || atk > 20) {
+            throw new Error("nem megfelelő a támadás mennyisége!");
+        }
+        else if (hp < 50 || hp > 100) {
+            throw new Error("nem megfelelő az élet mennyisége!");
+        }
+        
+        this.name = name;
+        this.atk = atk;
+        this.hp = hp;
+    }   
+}
---
+++ test/rat.test.ts
@@ -0,0 +1,23 @@
+import { describe, expect, test } from "vitest";
+import {Rat} from "../src/rat.ts";
+
+describe("Rat osztály", () => {
+    test("Sikeres létrehozás", () => {
+        expect(()=> {new Rat("Ödön",11,65)}).to.not.throw();
+    });
+    test("Üres név", () => {
+        expect(() => {new Rat("",11,70)}).to.throw();
+    });
+    test("kevéss atk", () => {
+        expect(() => {new Rat("Atis",9,70)}).to.throw();
+    });
+    test("sok atk", () => {
+        expect(() => {new Rat("Rózsi",50,61)}).to.throw();
+    })
+    test("kevéss hp", () => {
+        expect(() => {new Rat("Atis",15,49)}).to.throw();
+    });
+    test("sok hp", () => {
+        expect(() => {new Rat("Rózsi",50,120)}).to.throw();
+    });
+});
